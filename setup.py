bb800714697a71e7e7cd9f3c4f6e1da13e2a9fdb
Aleksey Sheludchenkov <aleshkashell@gmail.com>
2018-10-15 14:28:08 +0300

    fix version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'pyteamcity',
   packages = ['pyteamcity'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.1.2',
   description = 'Module for work with teamcity',
   author = 'Sheludchenkov Aleksey',
   author_email = 'aleshkashell@gmail.com',
   url = 'https://github.com/aleshkashell/pyteamcity',
-  download_url = 'https://github.com/aleshkashell/pyteamcity/tarball/0.1',
+  download_url = 'https://github.com/aleshkashell/pyteamcity/tarball/0.1.2',
   keywords = ['add', 'sub', 'tests', 'freeipa'], 
   classifiers = [],
 )
